483bd929efc1366471a8b153fc891f58093b78dc
Krzysztof Rosiek <k.rosiek@emys.co>
2021-07-17 20:05:46 +0200

    Added build_runner, updated cupertino icons, mockito. Implemented get concrete number trivia,

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,13 +1,13 @@
 import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tddcourse/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tddcourse/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:tddcourse/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'get_concrete_number_trivia_test.mocks.dart';
 
-class MockNumberTriviaRepository extends Mock
-    implements NumberTriviaRepository {}
-
+@GenerateMocks([NumberTriviaRepository])
 void main() {
   late MockNumberTriviaRepository mockNumberTriviaRepository;
   late GetConcreteNumberTrivia usecase;
@@ -27,7 +27,7 @@ void main() {
       when(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber))
           .thenAnswer((_) async => const Right(tNumberTrivia));
       // act
-      final result = await usecase(Params(number: tNumber));
+      final result = await usecase(const Params(number: tNumber));
       // assert
       expect(result, const Right(tNumberTrivia));
       verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
