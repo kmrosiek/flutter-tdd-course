56b46f01b1bcd904a079ecb5b03a360f573f5d21
Krzysztof Rosiek <k.rosiek@emys.co>
2021-07-17 19:47:55 +0200

    Added get concrete number trivia test - test fails due to null-safety changes in mockito - needs update.

---
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,37 @@
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tddcourse/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:tddcourse/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:tddcourse/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+  late GetConcreteNumberTrivia usecase;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  const tNumber = 1;
+  const tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
+  test(
+    'should get trivia for the number from the repository',
+    () async {
+      // arrange
+      when(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber))
+          .thenAnswer((_) async => const Right(tNumberTrivia));
+      // act
+      final result = await usecase(Params(number: tNumber));
+      // assert
+      expect(result, const Right(tNumberTrivia));
+      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+    },
+  );
+}
