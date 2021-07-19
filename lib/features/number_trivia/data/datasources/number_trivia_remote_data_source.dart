570c1bb558cbdd1aa7e9864e25ba1ae1843fdf7e
Krzysztof Rosiek <k.rosiek@emys.co>
2021-07-19 17:31:47 +0200

    Implemented exceptions, network_info, datasources contracts.

---
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../../core/error/exceptions.dart';
+import '../models/number_trivia_model.dart';
+
+abstract class NumberTriviaRemoteDataSource {
+  /// Calls the http://numbersapi.com/{number} endpoint.
+  ///
+  /// Throws a [ServerException] for all error codes.
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
+
+  /// Calls the http://numbersapi.com/random endpoint.
+  ///
+  /// Throws a [ServerException] for all error codes.
+  Future<NumberTriviaModel> getRandomNumberTrivia();
+}
