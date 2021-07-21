e2346e29a049184cd49cd0193529a3d942172d47
Krzysztof Rosiek <k.rosiek@emys.co>
2021-07-21 13:26:52 +0200

    Implemented Remote data source and its tests.

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -16,3 +16,32 @@ abstract class NumberTriviaRemoteDataSource {
   /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getRandomNumberTrivia();
 }
+
+class HttpNumberTriviaRemoteDataSource implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  HttpNumberTriviaRemoteDataSource({required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl('http://numbersapi.com/$number');
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('http://numbersapi.com/random');
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+}
