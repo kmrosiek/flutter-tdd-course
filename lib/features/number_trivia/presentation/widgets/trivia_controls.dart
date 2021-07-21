7da5d2266f8d9ea8c322e3b85cb4b07dbbb7df34
Krzysztof Rosiek <k.rosiek@emys.co>
2021-07-22 01:13:25 +0200

    Implemented GUI

---
+++ lib/features/number_trivia/presentation/widgets/trivia_controls.dart
@@ -0,0 +1,72 @@
+import 'package:tddcourse/features/number_trivia/presentation/bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _TriviaControlsState createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  final controller = TextEditingController();
+  String inputStr = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: controller,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a number',
+          ),
+          onChanged: (value) {
+            inputStr = value;
+          },
+          onSubmitted: (_) {
+            dispatchConcrete();
+          },
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: ElevatedButton(
+                child: const Text('Search'),
+                onPressed: dispatchConcrete,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: ElevatedButton(
+                child: const Text('Get random trivia'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[350],
+                  onPrimary: Colors.black,
+                ),
+                onPressed: dispatchRandom,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void dispatchConcrete() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context)
+        .add(GetTriviaForConcreteNumber(inputStr));
+  }
+
+  void dispatchRandom() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context).add(GetTriviaForRandomNumber());
+  }
+}
